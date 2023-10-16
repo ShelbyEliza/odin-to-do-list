c2493c2030ab1e7e63121f19d6f47e329177f055
ShelbyEliza <shelbyelizashipley@gmail.com>
2023-10-16 10:58:23 -0700

    Add notes on dom flow

--- src/nav.js
+++ src/nav.js
@@ -3,6 +3,7 @@ class Navbar {
 	constructor(element, className, parentElement) {
 		this.dom = document.createElement(element);
 		this.parentElement = parentElement;
+		this.activeTab = null;
 
 		if (className !== "") {
 			this.dom.classList.add(className);
@@ -22,6 +23,7 @@ class Navbar {
 	}
 	setActiveTab(tab) {
 		this.activeTab = tab;
+		console.log(tab);
 	}
 }
 
@@ -32,7 +34,10 @@ class NavButton extends Navbar {
 		this.dom.id = textContent.toLowerCase();
 	}
 }
-
+/**
+ * index.js defines navView as a new NavController instance
+ * NavController creates an instance of the Navbar: DOM element, nav, that holds the NavButtons)
+ * */
 class NavController {
 	constructor(parentEl) {
 		this.parentEl = parentEl;
@@ -41,15 +46,22 @@ class NavController {
 		this.createTab = new NavButton("button", "tab", this.nav.dom, "Create");
 
 		this.nav.addTab(this.homeTab);
-		this.nav.setActiveTab(this.homeTab);
 		this.nav.addTab(this.createTab);
+		this.nav.setActiveTab(this.homeTab);
+		console.log(this.homeTab);
+		console.log(this.nav.activeTab);
 	}
 	switchActiveTab(selectedTab) {
+		console.log(selectedTab.id, this.nav);
 		this.nav.allTabs.forEach((tab) => {
-			if (tab.dom.id === selectedTab.id) {
-				tab.dom.classList.add("active");
+			if (selectedTab.id !== this.nav.activeTab.id) {
+				if (tab.dom.id === selectedTab.id) {
+					tab.dom.classList.add("active");
+				} else {
+					tab.dom.classList.remove("active");
+				}
 			} else {
-				tab.dom.classList.remove("active");
+				console.log("no change");
 			}
 		});
 	}
